1896caadadddb2057dcda32372497318ab4c0a5f
Stephen Lashinski <slashinski@bpm105.local>
2019-03-23 16:10:54 -0500

    nx api work

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface Todo {
+  title: string;
+}
 @Component({
   selector: 'monoRepoEx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todos';
+  // title = 'Todos';
+  // todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe(t => (this.todos = t));
+  }
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`
+    });
+  }
 }
